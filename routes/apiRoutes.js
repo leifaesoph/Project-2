c3cd70e6fa8171b8599929cb871904cc92c723d2
ruiligao <ruiligao@gmail.com>
2019-04-14 13:16:08 -0700

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,18 +6,13 @@ module.exports = function (app) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
-    
     res.json("/users");
   });
 
   // Create a new user
   app.post("/api/signup", function (req, res) {
     db.Users.create(req.body).then(function (dbUsers) {
-      // res.json(dnUser) just for test
-
-      // res.redirect(307, "/api/login");
-      console.log("KKKKK");
-      res.json(dbUsers);
+      res.redirect("/users");
     }).catch(function (err) {
       console.log(err);
       res.json(err);
