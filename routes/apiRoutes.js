08ed4aba9a825bcca6e8a96ea428c228d6366f25
ruiligao <ruiligao@gmail.com>
2019-04-14 12:57:43 -0700

    login success functional
    sign up functional
    login failure not displayed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,32 @@
 var db = require("../models");
-
-module.exports = function(app) {
+var passport = require('passport');
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
+    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+    // So we're sending the user back the route to the members page because the redirect will happen on the front end
+    // They won't get this or even be able to access this page if they aren't authed
+    
+    res.json("/users");
   });
 
   // Create a new user
-  app.post("/api/users", function(req, res) {
-    db.Users.create(req.body).then(function(dbUsers){
+  app.post("/api/signup", function (req, res) {
+    db.Users.create(req.body).then(function (dbUsers) {
       // res.json(dnUser) just for test
+
+      // res.redirect(307, "/api/login");
+      console.log("KKKKK");
       res.json(dbUsers);
-      // nees to create the routs to user page
-      // res.redirect("/api/users/:id");
+    }).catch(function (err) {
+      console.log(err);
+      res.json(err);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
