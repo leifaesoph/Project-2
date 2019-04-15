6b38a148cd4e4ae2e0cc8cf6f3012dca8e431c81
ruiligao <ruiligao@gmail.com>
2019-04-14 22:27:12 -0700

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,12 @@ var db = require("../models");
 var passport = require('passport');
 module.exports = function (app) {
   // Get all examples
+  app.get("/api/logout", function(req, res){
+    console.log("TEST");
+    res.json("/");
+    // res.redirect("/");
+  });
+
   app.post("/api/login", passport.authenticate("local"), function (req, res) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
@@ -18,7 +24,30 @@ module.exports = function (app) {
       res.json(err);
     });
   });
-
+// logout
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+  //catch the user email
+  app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    }
+    else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      db.Users.findOne({
+        where: {
+          email: req.user.email
+        }
+      }).then(function(data){
+        console.log("KKKKK" +data.name)
+        res.json(data.name);
+      });
+    }
+  });
   // Delete an example by id
   app.delete("/api/examples/:id", function (req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
