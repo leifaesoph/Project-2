b260c0aac5e7a6f2a8296ab48e2a483437d51a2f
Turbocake64 <micahrabinowitz@gmail.com>
2019-04-16 16:18:19 -0700

    dom now populates with loans and debts

--- public/js/users.js
+++ public/js/users.js
@@ -1,4 +1,8 @@
 var user = null;
+var loanTransactionNumber = 0;
+var totalMoneyLent = 0;
+var debtTransactionNumber = 0;
+var totalMoneyBorrowed = 0;
 
 $(document).ready(function() {
     // This file just does a GET request to figure out which user is logged in
@@ -13,12 +17,36 @@ $(document).ready(function() {
 
     });
     $.get("/api/user_loans").then(function(data) {
-        console.log(data);
-      $(".UDIV").text(data);
+        console.log(data[0].borrowerName, data[0].dueDate, data[0].amount);
+        for(var i = 0; i < data.length; i++) {
+          loanTransactionNumber++;
+          totalMoneyLent += parseFloat(data[i].amount);
+          var newBtn=$("<button>");
+          $("#UDIV").append(newBtn);
+          newBtn.attr("class", "loanReminder");
+          newBtn.attr("value", data[i].borrowerName);
+          newBtn.text(data[i].borrowerName + " | " + data[i].dueDate + " | " + data[i].amount); 
+        };
+        $("#Up").prepend("Loans: " + loanTransactionNumber + " | ");
+        $("#Up").prepend("Total: $" + totalMoneyLent + " | ");
     });
     $.get("/api/user_debts").then(function(data) {
         console.log(data);
-      $(".ODIV").text(data);
+        console.log(data[0].lenderName, data[0].dueDate, data[0].amount);
+        for(var i = 0; i < data.length; i++) {
+          debtTransactionNumber++;
+          totalMoneyBorrowed += parseFloat(data[i].amount);
+          var newBtn=$("<button>");
+          $("#ODIV").append(newBtn);
+          newBtn.attr("class", "debtReminder");
+          newBtn.attr("value", data[i].lenderName);
+          newBtn.text(data[i].lenderName + " | " + data[i].dueDate + " | " + data[i].amount); 
+        };
+        $("#Op").append("Debts: " + debtTransactionNumber + " | ");
+        $("#Op").append("Total: $" + totalMoneyBorrowed + " | ");
+        $("#balanceDisplay").text(totalMoneyLent - totalMoneyBorrowed);
     });
+
   });
+
   
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -127,20 +127,53 @@ module.exports = function (app) {
 
   app.get("/api/user_loans", function (req, res) {
     console.log(req)
+    console.log(req.user);
     db.Transactions.findAll({
       where: {
-        lenderId: userData.id
+        lenderId: req.user.id
       }
+    }).then(function (data) {
+      res.json(data)
     });
+  });
 
-    app.get("/api/user_debts", function (req, res) {
-      console.log(req)
-      db.Transactions.findAll({
-        where: {
-          borrowerId: userData.id
-        }
-      });
-    })
-  }
-  )
+  app.get("/api/user_debts", function (req, res) {
+    console.log(req);
+    db.Transactions.findAll({
+      where: {
+        borrowerId: req.user.id
+      }
+    }).then(function (data) {
+      res.json(data)
+    });
+  });
+
+  // app.get("/api/new_debt_approve", function (req, res) {
+  //   console.log(req)
+  //   db.Transactions.findAll({
+  //     where: {
+  //       [Op.or]: [{BorrowerId: user.id}, {BorrowerApproval: null}]
+  //     }
+  //   })
+  // });
+
+  // app.put("/api/new_debt_confirm"), function (req, res) {
+  //   console.log(req)
+  //   db.Transaction.update({
+  //     where: {
+
+  //     }
+  //   })
+  // }
+
+  // app.get("/api/new_payment_approve", function (req, res) {
+  //   console.log(req)
+  //   db.Transactions.findAll({
+  //     where: {
+  //       [Op.or]: [{lenderId: userData.id}, {PayDate: notNull}]
+  //     }
+  //   })
+  // });
+
+
 };
